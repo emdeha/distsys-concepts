e18d3d2ca80c82c6567329655fbd829c5fc97304
Tsvetan Tsvetanov <tsvetan.tsvetanov@ridewithvia.com>
2020-04-20 14:22:51 +0300

    Makes participant save committed value.

--- two-phase-commit/participant.go
+++ two-phase-commit/participant.go
@@ -6,13 +6,23 @@ import (
 	"os"
 	"bufio"
 	"strings"
+	"io/ioutil"
 )
+
+var participantAddress string
 
 func main() {
 	ln, err := net.Listen("tcp", os.Args[1])
 	if err != nil {
 		panic(err)
 	}
+
+	participantAddress = os.Args[1]
+
+	// value records the proposed value which is later to be committed. It's
+	// currently shared among all connections because we don't allow for more
+	// than one coordinator in the network.
+	var value string
 
 	for {
 		conn, err := ln.Accept()
@@ -35,10 +45,13 @@ func main() {
 		case "propose":
 			fmt.Println("vote")
 			vote(conn)
+			value = requestParts[1]
 		case "commit\n":
-			fmt.Println("commit value")
+			fmt.Println("commit value", value)
+			commit(value)
 		case "abort\n":
 			fmt.Println("abort value")
+			abort(&value)
 		default:
 			fmt.Printf("invalid command: %s\n", cmd)
 		}
@@ -47,4 +60,15 @@ func main() {
 
 func vote(conn net.Conn) {
 	fmt.Fprintf(conn, "Ack\n")
+}
+
+func commit(value string) {
+	err := ioutil.WriteFile("./data/" + participantAddress, []byte(value), 0444)
+	if err != nil {
+		fmt.Errorf("commit: error writing file: %w", err)
+	}
+}
+
+func abort(value *string) {
+	*value = ""
 }
